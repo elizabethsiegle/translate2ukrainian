ece47383c3685d92a0b863678052853de33f087e
Lizzie Siegle <esiegle@brynmawr.edu>
2022-03-21 14:09:39 -0700

    first commit

---
+++ functions/hello-world.js
@@ -0,0 +1,29 @@
+const LanguageTranslatorV3 = require('ibm-watson/language-translator/v3');
+const { IamAuthenticator } = require('ibm-watson/auth');
+exports.handler = async function(context, event, callback) {
+  const twiml = new Twilio.twiml.MessagingResponse();
+  const message = twiml.message();
+  const msgToTranslate = event.Body.toLowerCase().trim();
+  const languageTranslator = new LanguageTranslatorV3({
+    version: '2018-05-01',
+    authenticator: new IamAuthenticator({
+      apikey: context.WATSON_KEY,
+    }),
+    serviceUrl: 'https://api.us-south.language-translator.watson.cloud.ibm.com/instances/b332e9a7-0fe7-41b2-ba6e-5bcabe16992c',
+  });
+  const translateParams = {
+    text: msgToTranslate,
+    //modelId: 'en-uk', //Ukrainian model
+    target:'uk'
+  };
+  languageTranslator.translate(translateParams)
+    .then(translationResult => {
+      message.body(`"${msgToTranslate}" translated to Ukrainian is: ${translationResult.result.translations[0].translation}`);
+      callback(null, twiml);
+    })
+    .catch(err => {
+      console.log('error:', err);
+      message.body(err);
+      callback(null, twiml);
+    });
+  };
